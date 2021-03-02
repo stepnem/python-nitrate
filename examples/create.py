stepnem/python-nitrate

commit 90b1d68227dfdc73b3eaffc33ace06e4610be1df

Migrate the code to Python 3
Co-authored-by: Mr. Senko <atodorov@mrsenko.com>
Co-authored-by: Lumír Balhar <lbalhar@redhat.com>

examples/create.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 """
 Basic Example
 

examples/matrix.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import re, sys, optparse
 from nitrate import *

Makefile
@@ -59,4 +59,4 @@ push: packages
 		$(PUSH_URL)/download
 
 clean:
-	rm -rf $(TMP) build dist nitrate.egg-info source/*.pyc
+	rm -rf $(TMP) build dist nitrate.egg-info source/*.pyc source/__pycache__

python-nitrate.spec
@@ -1,6 +1,6 @@
 Name: python-nitrate
-Version: 1.4
-Release: 1%{?dist}
+Version: 1.5
+Release: 0%{?dist}
 
 Summary: Python API for the Nitrate test case management system
 Group: Development/Languages
@@ -11,7 +11,7 @@ Source0: https://github.com/psss/python-nitrate/releases/download/%{version}/pyt
 
 BuildArch: noarch
 BuildRequires: git
-BuildRequires: python2-devel
+BuildRequires: python3-devel python3-six
 
 %global _description\
 python-nitrate is a Python interface to the Nitrate test case\
@@ -22,16 +22,16 @@ line interpreter (useful for fast debugging and experimenting).
 
 %description %_description
 
-%package -n python2-nitrate
+%package -n python3-nitrate
 Summary: %summary
 %if 0%{?rhel}
 Requires: python-gssapi python-psycopg2
 %else
-Requires: python2-gssapi python2-psycopg2
+Requires: python3-gssapi python3-psycopg2
 %endif
-%{?python_provide:%python_provide python2-nitrate}
+%{?python_provide:%python_provide python3-nitrate}
 
-%description -n python2-nitrate %_description
+%description -n python3-nitrate %_description
 
 %prep
 %autosetup -S git
@@ -41,18 +41,21 @@ Requires: python2-gssapi python2-psycopg2
 %install
 mkdir -p %{buildroot}%{_bindir}
 mkdir -p %{buildroot}%{_mandir}/man1
-mkdir -p %{buildroot}%{python2_sitelib}/nitrate
+mkdir -p %{buildroot}%{python3_sitelib}/nitrate
 install -pm 755 source/nitrate %{buildroot}%{_bindir}
-install -pm 644 source/*.py %{buildroot}%{python2_sitelib}/nitrate
+install -pm 644 source/*.py %{buildroot}%{python3_sitelib}/nitrate
 install -pm 644 docs/*.1.gz %{buildroot}%{_mandir}/man1
 
-%files -n python2-nitrate
+%files -n python3-nitrate
 %{_mandir}/man1/*
 %{_bindir}/nitrate
-%{python2_sitelib}/*
+%{python3_sitelib}/*
 %doc COPYING README examples
 
 %changelog
+* Mon Nov 04 2019 Martin Zeleny <mzeleny@redhat.com> 1.5-0
+- Ported to Python 3
+
 * Mon Nov 05 2018 Petr Šplíchal <psssssss@gmail.com> 1.4-1
 - Update specfile to new python packaging standards
 - Performance improvement for TestPlan.sortkey

setup.py
@@ -30,6 +30,7 @@ setup(name='nitrate',
       install_requires=[
           'gssapi',
           'psycopg2',
+          'six',
           ],
       url='https://psss.fedorapeople.org/python-nitrate/',
       classifiers=[

source/__init__.py
@@ -60,7 +60,7 @@ For details see pydoc documentation for individual modules:
     nitrate.teiid ........ Teiid support
     nitrate.tests ........ Test suite
     nitrate.utils ........ Utilities
-    nitrate.xmlrpc ....... XMLRPC driver
+    nitrate.xmlrpc_driver. XMLRPC driver
 """
 
 # Nitrate objects
@@ -93,7 +93,7 @@ from nitrate.config import (
         NEVER_EXPIRE, NEVER_CACHE, MULTICALL_MAX)
 
 # Data communication exceptions
-from nitrate.xmlrpc import NitrateError
+from nitrate.xmlrpc_driver import NitrateError
 from nitrate.teiid import TeiidError
 
 # Persistent cache and Multicall support

source/base.py
@@ -37,14 +37,15 @@ For the complete list of available operators see Django documentation:
 https://docs.djangoproject.com/en/dev/ref/models/querysets/#field-lookups
 """
 
+import six
 import datetime
 import nitrate.config as config
 import nitrate.utils as utils
-import nitrate.xmlrpc as xmlrpc
+import nitrate.xmlrpc_driver as xmlrpc_driver
 import nitrate.teiid as teiid
 
 from nitrate.config import log, Config
-from nitrate.xmlrpc import NitrateError
+from nitrate.xmlrpc_driver import NitrateError
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #  Internal Utilities
@@ -115,7 +116,7 @@ def _idify(id):
         result = []
         while id > 0:
             remainder = id % config._MAX_ID
-            id = id / config._MAX_ID
+            id = id // config._MAX_ID
             result.append(int(remainder))
         result.reverse()
         return result
@@ -195,13 +196,13 @@ class Nitrate(object):
                     Config().nitrate.url))
             # Plain authentication if username & password given
             try:
-                Nitrate._connection = xmlrpc.NitrateXmlrpc(
+                Nitrate._connection = xmlrpc_driver.NitrateXmlrpc(
                         Config().nitrate.username,
                         Config().nitrate.password,
                         Config().nitrate.url).server
             # Kerberos otherwise
             except AttributeError:
-                Nitrate._connection = xmlrpc.NitrateKerbXmlrpc(
+                Nitrate._connection = xmlrpc_driver.NitrateKerbXmlrpc(
                         Config().nitrate.url).server
 
         # Return existing connection
@@ -221,7 +222,7 @@ class Nitrate(object):
     def _cache_lookup(cls, id, **kwargs):
         """ Look up cached objects, return found instance and search key """
         # ID check
-        if isinstance(id, int) or isinstance(id, basestring):
+        if isinstance(id, int) or isinstance(id, six.string_types):
             return cls._cache[id], id
 
         # Check injet (initial object dictionary) for id
@@ -243,7 +244,7 @@ class Nitrate(object):
         if isinstance(id, Nitrate):
             return id._fetched is not None
         # Check for presence in cache, make sure the object is fetched
-        if isinstance(id, int) or isinstance(id, basestring):
+        if isinstance(id, int) or isinstance(id, six.string_types):
             return id in cls._cache and cls._cache[id]._fetched is not None
         # Run recursively for each given id/object if list given
         if isinstance(id, list) or isinstance(id, set):
@@ -270,7 +271,7 @@ class Nitrate(object):
         if isinstance(id_or_inject, dict):
             inject = id_or_inject
         # Object identified by name
-        elif isinstance(id_or_inject, basestring):
+        elif isinstance(id_or_inject, six.string_types):
             name =  id_or_inject
         # Regular object id
         else:
@@ -323,7 +324,7 @@ class Nitrate(object):
             if isinstance(id, int):
                 log.cache("Caching {0} ID#{1}".format(cls.__name__, id))
                 cls._cache[id] = new
-            elif isinstance(id, basestring) or "name" in kwargs:
+            elif isinstance(id, six.string_types) or "name" in kwargs:
                 log.cache("Caching {0} '{1}'".format(
                         cls.__name__, (id or kwargs.get("name"))))
             return new
@@ -349,7 +350,10 @@ class Nitrate(object):
 
     def __str__(self):
         """ Provide ascii string representation """
-        return utils.ascii(unicode(self))
+        if six.PY2:
+            return utils.ascii(unicode(self))
+        else:
+            return self.__unicode__()
 
     def __unicode__(self):
         """ Short summary about the connection """

source/cache.py
@@ -116,7 +116,8 @@ import pickle
 import atexit
 import datetime
 import tempfile
-import xmlrpclib
+from six.moves import xmlrpc_client as xmlrpclib
+
 import nitrate.immutable as immutable
 import nitrate.mutable as mutable
 import nitrate.containers as containers
@@ -189,7 +190,8 @@ class Cache(object):
     def __new__(cls, *args, **kwargs):
         """ Make sure we create a single instance only """
         if not cls._instance:
-            cls._instance = super(Cache, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(Cache, cls).__new__(cls)
+            cls._instance.__init__(*args, **kwargs)
         return cls._instance
 
     def __init__(self, filename=None):
@@ -278,7 +280,7 @@ class Cache(object):
             output_file.close()
             os.rename(output_file.name, self._filename)
             log.debug("Persistent cache successfully saved")
-        except IOError, error:
+        except IOError as error:
             log.error("Failed to save persistent cache ({0})".format(error))
 
     def load(self):
@@ -298,7 +300,7 @@ class Cache(object):
         except EOFError:
             log.cache("Cache file empty, will fill it upon exit")
             return
-        except (IOError, zlib.error), error:
+        except (IOError, zlib.error) as error:
             if getattr(error, "errno", None) == 2:
                 log.warn("Cache file not found, will create one on exit")
                 return
@@ -370,7 +372,7 @@ class Cache(object):
         try:
             log.cache("Removing cache lock {0}".format(self._lock))
             os.remove(self._lock)
-        except OSError, error:
+        except OSError as error:
             log.error("Failed to remove the cache lock {0} ({1})".format(
                     self._lock, error))
 
@@ -422,7 +424,7 @@ class Cache(object):
                     listed([klass.__name__ for klass in classes])))
         # For each class re-initialize objects and remove from index
         for current_class in classes:
-            for current_object in current_class._cache.itervalues():
+            for current_object in current_class._cache.values():
                 # Reset the object to the initial state
                 current_object._init()
             current_class._cache = {}

commit f2c6f22734327eea9f3d07f211a8ea5f7f75405e

Make examples usable with Python 3
That seems to have been the intention, as the shebang already assumes
python3.

I haven't tried actually running create.py (it looks more like just
a collection of examples), but matrix.py now does work as a python3
script.

Fixes: 90b1d68227df ("Migrate the code to Python 3")

examples/create.py
@@ -19,7 +19,7 @@ nitrate.set_color_mode(nitrate.COLOR_AUTO)
 # Initialize an existing test plan
 info("Initializing an existing test plan")
 general_plan = nitrate.TestPlan(3781)
-print general_plan
+print(general_plan)
 
 # Logging, catching errors
 nitrate.set_log_level(nitrate.LOG_INFO)
@@ -27,14 +27,14 @@ info("Initializing a bad test plan")
 try:
     log.info("Inspecting the test plan")
     bad_plan = nitrate.TestPlan(1)
-    print bad_plan
-except nitrate.NitrateError, error:
+    print(bad_plan)
+except nitrate.NitrateError as error:
     log.error("Bad test plan id ({0})".format(error))
 
 # Iterate over all test plan's test cases
 info("Inspecting test plan's test cases")
 for testcase in general_plan:
-    print testcase
+    print(testcase)
 
 # Create a new test plan
 info("Creating a new test plan")
@@ -73,7 +73,7 @@ testcase.update()
 # List all general plan's children including their status
 info("Listing child test plans of the general test plan")
 for testplan in general_plan.children:
-    print testplan, testplan.status
+    print(testplan, testplan.status)
 
 # Create a new test run
 info("Creating a new test run")
@@ -87,11 +87,11 @@ info("Scheduling performance tests")
 for caserun in testrun:
     if (caserun.status == nitrate.Status("IDLE")
             and str(caserun.testcase.category) == "Performance"):
-        print caserun.testcase.script, caserun.testcase.arguments
+        print(caserun.testcase.script, caserun.testcase.arguments)
         caserun.status = nitrate.Status("RUNNING")
         caserun.update()
 
 # Check case run status of the whole test run
 info("Checking case run status")
 for caserun in testrun:
-    print caserun
+    print(caserun)

examples/matrix.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     options = parser.parse_args()[0]
 
     testplan = TestPlan(options.plan)
-    print "Checking results of {0}:\nTest runs: {1}".format(testplan,
-            ", ".join([run.identifier for run in testplan.testruns]))
+    print("Checking results of {0}:\nTest runs: {1}".format(testplan,
+            ", ".join([run.identifier for run in testplan.testruns])))
 
-    for testcase in sorted(testplan.testcases, key=lambda x: x.script):
+    for testcase in sorted(testplan.testcases, key=lambda x: x.script or ""):
         for testrun in testplan.testruns:
             for caserun in testrun.caseruns:
                 if caserun.testcase.id == testcase.id:
-                    print caserun.status.shortname,
+                    print(caserun.status.shortname)
                     break
             else:
-                print "....",
-        print "- {0} - {1}".format(str(testcase.tester).ljust(8), testcase)
+                print("....")
+        print("- {0} - {1}".format(str(testcase.tester).ljust(8), testcase))